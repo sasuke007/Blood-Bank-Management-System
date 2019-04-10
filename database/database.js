77bc336207e9a115a6e0d36eae8135adb20955b2
NikhilMotwanikt13 <nikhilmotwanikt13@gmail.com>
2019-04-10 13:18:08 +0530

    userlogin page added

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Rohit:mongodb@@7@cluster0-liiq2.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://Nikhil:mongodb@@7@cluster0-liiq2.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 module.exports.client=client;
