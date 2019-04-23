483960bba8bdb65da533260733ea05a1d9948fef
NikhilMotwanikt13 <nikhilmotwanikt13@gmail.com>
2019-04-23 13:03:34 +0530

    adminhome page added

---
+++ javascript/userlogin_javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("signup_button").onclick=redirect();
+function redirect(){
+    document.getElementById("userlogin_heading").innerHTML("hello");
+    window.location="signup.html";
+}
